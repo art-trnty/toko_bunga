fa577ff5dff5584d005e12bf9bea2b4c9b9eb980
Rara Ananta Bunga <164327639+art-trnty@users.noreply.github.com>
2024-12-08 11:43:47 +0700

    Data dan models
    menambahkan file data dan models untuk halaman detail stok produk yang dijual disebuah toko bunga

---
+++ lib/data/product_data.dart
@@ -0,0 +1,26 @@
+final List<Map<String, String>> products = [
+  {
+    "name": "Peperomia Houseplant",
+    "price": "Rp 100.000",
+    "image": "assets/houseplant-peperomia.jpg",
+    "availability": "Tersedia"
+  },
+  {
+    "name": "Crassula Houseplant",
+    "price": "Rp 120.000",
+    "image": "assets/houseplant-crassula.jpg",
+    "availability": "Tersedia"
+  },
+  {
+    "name": "Vitamin B",
+    "price": "Rp 150.000",
+    "image": "assets/vitamin-b.jpg",
+    "availability": "Tersedia"
+  },
+  {
+    "name": "Asplenium Houseplant",
+    "price": "Rp 150.000",
+    "image": "assets/houseplant-asplenium.jpg",
+    "availability": "Tidak Tersedia"
+  },
+];
