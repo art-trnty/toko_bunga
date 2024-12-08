3a69604e60553e375dbef051a755f32fc4b2cff1
Rara Ananta Bunga <164327639+art-trnty@users.noreply.github.com>
2024-12-08 12:44:18 +0700

    Files coding halaman detail produk toko
    coding halaman menambahkan detail produk toko bunga yang dijual pada toko tersebut

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toko_bunga/screens/cart_screens.dart';
+import 'package:toko_bunga/screens/store_detail_screens.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CartScreen(),
+      //home: CartScreen(),
+      home: StoreDetailScreen(),
     );
   }
 }
