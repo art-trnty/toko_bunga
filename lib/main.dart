09f1487868ef59ff7a2d7868f8230f98d55b9f92
kiarra <beneciakiarra_2226240052@mhs.mdp.ac.id>
2024-12-15 18:57:30 +0700

    delete
    delete beberapa screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:toko_bunga/screens/DaftarTokoScreen.dart';
 import 'package:toko_bunga/screens/HomeScreen.dart' as home;
 import 'package:toko_bunga/screens/HomeScreen.dart';
-import 'package:toko_bunga/screens/SearchScreen.dart' as search;
 import 'package:toko_bunga/screens/SignInScreen.dart';
 import 'package:toko_bunga/screens/cart_screens.dart';
-import 'package:toko_bunga/screens/store_detail_screens.dart';
 import 'package:toko_bunga/screens/Notifikasi_pembayaran.dart';
 import 'package:toko_bunga/screens/Pembayaran_Screen.dart';
 
